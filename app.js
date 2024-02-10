df6f74a7a3fac0513271e0bcf93b6aed9455ddc0
maks-im-lev <158667874+maks-im-lev@users.noreply.github.com>
2024-02-10 14:21:06 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+// moving sideMenu
+var sidemenu = document.getElementById("sidemenu");
+function openmenu() {
+  sidemenu.style.right = "0";
+}
+function closemenu() {
+  sidemenu.style.right = "-200px";
+}
+
+// mail
+
+// const scriptURL = '<https://script.google.com/macros/s/AKfycbxza7MoNOGi0ASlaXpxF0lKPh38aA99Tq9w-1cMO9CoEKARPMPE0DqCJbAsCx51cyrd/exec>'
+// const form = document.forms['submit-to-google-sheet']
+
+// form.addEventListener('submit', e => {
+//   e.preventDefault()
+//   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+//     .then(response => console.log('Success!', response))
+//     .catch(error => console.error('Error!', error.message))
+// })
+
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_0ehof3p";
+  const templateID = "template_aa0bzbs";
+
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      alert("your message sent successfully");
+    })
+    .cathch((err) => console.log(err));
+}
